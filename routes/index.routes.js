a042cc359ec8d3553597f02ad6b9e08120540f19
Pablo de Andres <pablodeandresolano@gmail.com>
2023-05-09 10:56:06 +0200

    Add user Routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,4 +5,8 @@ router.get('/', (req, res, next) => {
     res.json('Funcionando correctamente');
 });
 
+const userRoutes = require('./user.routes.js')
+router.use("/user", userRoutes)
+
+
 module.exports = router;
