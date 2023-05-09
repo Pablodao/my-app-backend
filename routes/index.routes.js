3075bf54c66d28ae7623704e02c966582e056d6f
Pablo de Andres <pablodeandresolano@gmail.com>
2023-05-09 10:57:24 +0200

    Add pet Routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -8,5 +8,7 @@ router.get('/', (req, res, next) => {
 const userRoutes = require('./user.routes.js')
 router.use("/user", userRoutes)
 
+const petRoutes = require('./pet.routes.js')
+router.use("/pet", petRoutes)
 
 module.exports = router;
